62c39a81556e4ba3fd8187c0fe935fbcbd1d3303
Watheq Attrah <novaatrah@gmail.com>
2023-05-09 14:43:12 -0700

    Add db.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Add EventListener to Button
+
+
+//callback function for event listener
+
+
+
+//add car to the car list
+
+
+
+//take car from (form) and add it to page
+
+
+
+//load my cars list from json file
