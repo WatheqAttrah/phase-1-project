aa3ed57c3e95fbda78ce42c50725534e31be0b62
Watheq Attrah <novaatrah@gmail.com>
2023-05-10 10:08:36 -0700

    add function

--- index.js
+++ index.js
@@ -18,7 +18,24 @@ document.querySelector('form').reset()
 
 
 //add car to the car list
+function renderOneCar(car){
+    let card = document.createElement("li")
+    card.innerHTML = `
+         <div>
+            <h4><p>Car Make:  ${car.make}</p></h4>
+            <h4><p>Car Model: ${car.model}</p></h4>
+            <h4><p>Car year: ${car.year}</p></h4>
+            <h4><p>Trim: ${car.trim}</p></h4>
 
+         <button type="click">Remove From List</button>
+         </div> `;
+
+
+//add new car to my car list and append it to ul
+document.querySelector('ul').appendChild(card)
+let btn = document.querySelector('div ul')
+btn.addEventListener('click', e=>e.target.parentNode.parentNode.remove())
+}
 
 
 //take car from (form) and add it to page
