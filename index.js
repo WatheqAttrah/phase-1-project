5717d7c78e1c1e4c6c1b4934a6f46234cb728db5
Watheq Attrah <novaatrah@gmail.com>
2023-05-13 21:13:52 -0700

    modify

--- index.js
+++ index.js
@@ -1,58 +1,84 @@
 //Add EventListener to Button
-document.querySelector('form').addEventListener('submit', handleSubmit);
+document.querySelector('form').addEventListener('submit', handleSubmit)
 
-//callback function for event listener
+//callback function for eventlistener
 function handleSubmit(e){
     e.preventDefault()
     let carShow = {
         make:e.target.make.value,     
-        model:e.target.model.value, 
+        model:e.target.model.value,  
         year:e.target.year.value,
         trim:e.target.trim.value,
     }
 renderOneCar(carShow)
+//Using reste function to prevent redirect
 document.querySelector('form').reset()
+addCar(carShow);
 }
+
+
+
 //add car to the car list
 function renderOneCar(car){
     let card = document.createElement("li")
     card.innerHTML = `
          <div>
-            <h4><p>Car Make:  ${car.make}</p></h4>
-            <h4><p>Car Model: ${car.model}</p></h4>
-            <h4><p>Car year: ${car.year}</p></h4>
-            <h4><p>trim: ${car.trim}</p></h4>
+            <p>Car Make:  ${car.make}</p>
+            <p>Car Model: ${car.model}</p>
+            <p>Car year: ${car.year}</p>
+            <p>trim: ${car.trim}</p>
 
-         <button type="click">Remove From List</button>
+            <button type="click">Remove From List</button>
          </div> `;
+
 //add new car to my car list and append it to Unordered List
-document.querySelector('ul').appendChild(card);
-let btn = document.querySelector('div ul');
-btn.addEventListener('click', e=>e.target.parentNode.parentNode.remove());
-}
+document.querySelector('ul').appendChild(card)
 
-
-//contact json server and load my cars list json file
-function getAllcars(){
-    fetch("http://localhost:3000/carShow")
-    .then(response =>response.json())
-    .then(cars => cars.forEach(car => renderOneCar(car)))
+//Remove car from list 
+//let btn = document.querySelector('div ul');
+card.querySelector('#remove-btn').addEventListener('click',()=>{
+    card.remove();
+    removeCar(car.id);
+})
 }
+// btn.addEventListener('click', e=>e.target.parentNode.parentNode.remove())
+// //Remove car fetch function 
+// removeCar(car.id)
 
-//take car from (form) and add it to page
-function addCar(carShow){
-    fetch("http://localhost:3000/carShow",{
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(carShow)
-    })
-    .then(response=>response.json())
-    .then(car=>renderOneCar(car))
-};
-//load my cars list from json file
-function initialize(){
-    getAllcars()
-}
-initialize();
+// //contact json server and load my cars list json file
+// // function getAllCars(){
+// //     fetch("http://localhost:3000/carShow")
+// //     .then(response =>response.json())
+// //     .then(cars => cars.forEach(car => renderOneCar(car)))
+// // }
+// //function get cars
+// function getCars(){
+//     fetch('http://localhost:3000/carShow')
+//     .then(response=>response.json())
+//     .then(cars=>cars.forEach(car=>renderOneCar(car)))
+// }
+
+// //take car from (form) and add it to page
+// function addCar(carShow){
+//     fetch('http://localhost:3000/carShow',{
+//         method: 'POST',
+//         headers:{
+//             'Content-Type': 'application/json'
+//         },
+//     body: JSON.stringify(carShow)
+//     })
+//     .then(response=>response.json())
+//     .then(car=>renderOneCar(car))
+// }
+
+// //Remove a car form the object carShow
+// function removeCar(id){
+//     fetch(`http://localhost:3000/${id}`,{
+//         method:'DELETE',
+//         headers:{
+//             'Content-Type':'application/json'
+//         }
+//     })
+//     .then(response=>response.json())
+//     .then(can=>console.log(car))
+// }
