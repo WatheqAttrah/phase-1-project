3ffbaf4224a48fbdbcb2199decf9c806be51540d
Watheq Attrah <novaatrah@gmail.com>
2023-05-21 21:33:39 -0700

    all update

--- index.js
+++ index.js
@@ -1,70 +1,77 @@
-//Add EventListener to Button
-document.querySelector('form').addEventListener('submit',handleSubmit)
-//callback function for eventlistener
+//Add EventListener to the "form" Submit btn
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
 function handleSubmit(e){
-    e.preventDefault()//Prevent to re-direct the from 
-    const carShow = {
+    const carShow={
         make:e.target.make.value,     
         model:e.target.model.value,  
         year:e.target.year.value,
-        trim:e.target.trim.value,
+        trim:e.target.trim.value
     }
-renderOneCar(carShow)
-//Using reset function to prevent redirect
-document.querySelector('form').reset()
-addCar(carShow);//add the car to the object 
+addCar(carShow);
+renderOneCar(carShow);
+document.querySelector("form").reset()
+e.preventDefault()
 }
-//add car to the car list
+
 function renderOneCar(car){
     const card = document.createElement("li")
     card.innerHTML = `
-         <div>
-            <p>Car id:${car.id}</p>
-            <p>Car Make:  ${car.make}</p>
-            <p>Car Model: ${car.model}</p>
-            <p>Car year: ${car.year}</p>
-            <p>trim: ${car.trim}</p>
-            <button type="click">Remove From List</button>
-         </div> `;
-//Append  the car to child inside the DOM
-document.querySelector('ul').appendChild(card)
-//Remove from list 
-let btn = card.querySelector('div ul li div')//Remove button
-btn.addEventListener('click', ()=>{
-    card.remove()
-    removeCar(car.id)
-    })
+    <div>
+    <p>Car Make:  ${car.make}</p>
+    <p>Car Model: ${car.model}</p>
+    <p>Car Year: ${car.year}</p>
+    <p>Car Trim: ${car.trim}</p>
+    <p>Car Id:${car.id}</p>
+    
+    <button type="click">Remove From List</button>
+    </div>
+    `;
 }
-//GET FETCH FROM JSON FILE
+document.querySelector("ul").appendChild(card)
+
+const btn=card.querySelector("btn")
+btn.addEventListener("click",()=>{
+    card.remove();
+    removeCar(car.id)
+});
+
 function fetchCars(){
-    fetch('http://localhost:3000/carShow')
+    fetch("http://localhost:3000/carShow")
     .then(response=>response.json())
     .then(cars=>cars.forEach(car=>renderOneCar(car)))
 }
-
-//POST FETCH TO JSON OBJECT
 function addCar(carShow){
-    fetch('http://localhost:3000/carShow',{
-        method: 'POST',
+    fetch("http://localhost:3000/carShow",{
+        method:"POST",
         headers:{
-            'Content-Type': 'application/json'
+            "Content-Type":"application/json"
         },
-    body: JSON.stringify(carShow)
-    })
-    .then(response=>response.json())
-    .then(car=>renderOneCar(car));
+        body:JSON.stringify(carShow)
+        })
+        .then(response=>response.json())
+        .then(car=>console.log(car))
+
 }
-//DELETE FETCH FROM JSON OBJECT
+
+
+
 function removeCar(id){
     fetch(`http://localhost:3000/carShow/${id}`,{
         method:'DELETE',
         headers:{
-            'Content-Type':'application/json'
-        }
+            "Content-Type":"application/json"
+        },
     })
     .then(response=>response.json())
     .then(car=>console.log(car))
-}
+};
+
+
+
+
+
+
 //TO READ DATA FROM JSON FILE OR API
 function initialize(){
     fetchCars()
